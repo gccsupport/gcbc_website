8e617fbfe5dee9b558740b466be00b3beb2a43ef
Xiao Bo <garkan@protonmail.com>
2020-08-17 17:05:14 +0800

    选择语言样式

--- js/header.js
+++ js/header.js
@@ -11,3 +11,25 @@ function hideView() {
 		arguments[i].style.display = "none";
 	}
 }
+
+function hoverTitleOption() {
+	var length = arguments.length;
+	for (var i = 0; i < length; i++) {
+		if (i + 1 == length) {
+			arguments[i].style.top = "0px";
+		} else {
+			arguments[i].style.display = "block";
+		}
+	}
+}
+
+function hoverNotTitleOption() {
+	var length = arguments.length;
+	for (var i = 0; i < length; i++) {
+		if (i + 1 == length) {
+			arguments[i].style.top = "-4px";
+		} else {
+			arguments[i].style.display = "none";
+		}
+	}
+}
