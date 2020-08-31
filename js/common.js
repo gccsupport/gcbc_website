943d1228a8fd244e0590fde7ec6c4f1bec9eb8e7
Xiao Bo <garkan@protonmail.com>
2020-08-31 11:25:35 +0800

    DS孵化器标题动画增高

---
+++ js/common.js
@@ -0,0 +1,9 @@
+function languageHover(root) {
+	console.log("hover")
+	root.style.height = "130px";
+}
+
+function languageHoverOut(root) {
+	console.log("hoverOut")
+	root.style.height = "90px";
+}
