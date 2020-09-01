440f316651040f560722336d0c56b2106f3fab7a
Xiao Bo <garkan@protonmail.com>
2020-09-01 16:44:11 +0800

    使用 include 的方式引入公共布局

--- js/common.js
+++ js/common.js
@@ -7,3 +7,21 @@ function languageHoverOut(root) {
 	console.log("hoverOut")
 	root.style.height = "90px";
 }
+
+// 鼠标移进去
+function videoPlayback(img, imgPlayBtn, imgPlayLogo, video) {
+	console.log("hover了")
+	video.play();
+	imgPlayBtn.style.display = "none";
+	imgPlayLogo.style.display = "none";
+	img.style.display = "none";
+}
+
+//鼠标离开
+function videoStopped(video, imgPlayBtn, imgPlayLogo, img) {
+	console.log("hover移出去了")
+	video.pause();
+	imgPlayBtn.style.display = "inline";
+	imgPlayLogo.style.display = "inline";
+	img.style.display = "inline";
+}
