98c7d871ecb275ed62e641fb8391bbe35a6d7014
smkjios <garkios@protonmail.con>
2020-09-14 17:29:31 +0800

    添加回滚到顶部操作,更新首页产品中心图片和关于我们的轮播;

--- js/common.js
+++ js/common.js
@@ -73,3 +73,43 @@ function setScrollBackgroundColorChange(label) {
 	//滚动滑轮触发scrollFunc方法 //ie 谷歌 
 	window.onmousewheel = document.onmousewheel = scrollFunc;
 }
+
+//   页面动画滚动到顶部.
+function pageScroll() {
+	//把内容滚动指定的像素数（第一个参数是向右滚动的像素数，第二个参数是向下滚动的像素数）
+	window.scrollBy(0, -100);
+	//延时递归调用，模拟滚动向上效果
+	scrolldelay = setTimeout('pageScroll()', 10);
+	//获取scrollTop值，声明了DTD的标准网页取document.documentElement.scrollTop，否则取document.body.scrollTop；因为二者只有一个会生效，另一个就恒为0，所以取和值可以得到网页的真正的scrollTop值
+	var sTop = document.documentElement.scrollTop + document.body.scrollTop;
+	//判断当页面到达顶部，取消延时代码（否则页面滚动到顶部会无法再向下正常浏览页面）
+	if (sTop == 0) clearTimeout(scrolldelay);
+}
+
+
+function setbackTopHiden(img) {
+	var scrollFunc = function(e) {
+		e = e || window.event;
+		console.log('开始滚动');
+		if (e.wheelDelta) { //第一步：先判断浏览器IE，谷歌滑轮事件 
+			if (document.documentElement.scrollTop >= 500) {
+				img.style.display = "inline";
+			} else {
+				img.style.display = "none";
+			}
+		} else if (e.detail) { //Firefox滑轮事件 
+			if (document.documentElement.scrollTop >= 500) {
+				img.style.display = "inline";
+			} else {
+				img.style.display = "none";
+			}
+		}
+	}
+	//给页面绑定滑轮滚动事件 
+	if (document.addEventListener) { //firefox 
+		document.addEventListener('DOMMouseScroll', scrollFunc, true);
+	}
+	//滚动滑轮触发scrollFunc方法 //ie 谷歌 
+	window.onmousewheel = document.onmousewheel = scrollFunc;
+
+}
